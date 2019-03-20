5e06e0eefdfca1541e51e503b73ea7598cff5ea7
retriable <retriable@retriable.com>
2019-03-21 01:33:56 +0800

    no message

--- DeepLinkKitExtension tvOS Example/ViewController.m
+++ DeepLinkKitExtension tvOS Example/ViewController.m
@@ -6,9 +6,13 @@
 //  Copyright © 2019 retriable. All rights reserved.
 //
 
+@import DeepLinkKitExtension;
+
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic,strong)DPLDeepLinkRouter *router;
 
 @end
 
@@ -16,6 +20,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.router=[[DPLDeepLinkRouter alloc]init];
+    self.router[@"https://*.google.com/root"]=^(DPLDeepLink* link){
+        
+    };
+    
+    [self.router handleURL:[NSURL URLWithString:@"https://www.google.com/root"] parameters:nil withCompletion:^(BOOL handled, NSError *error) {
+        
+    }];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
